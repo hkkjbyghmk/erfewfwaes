679561b37a1182f971dfa00e03da0c80918c6881
hkkjbyghmk <69396490+hkkjbyghmk@users.noreply.github.com>
2024-09-22 21:40:34 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,41 +1,34 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import *
 import os
 
 app = Flask(__name__)
 
-BASE_DIR = r'C:/EuroShark'  # Novo diretório base
-
-# Caminho completo para o arquivo config.txt no novo diretório base
-CONFIG_FILE_PATH = os.path.join(BASE_DIR, 'config.txt')
-
-def check_create_config_file():
-    # Verifica se o arquivo config.txt existe, senão cria um novo
-    if not os.path.exists(CONFIG_FILE_PATH):
-        with open(CONFIG_FILE_PATH, 'w') as file:
-            file.write('Venda = ""\nCompra = ""\n')
+# Definindo os valores diretamente no código
+VENDA_VALUE = 27  # Valor para "Venda"
+COMPRA_VALUE = -12  # Valor para "Compra"
+ADMIN_CODE = '7851'  # Código para liberar o administrador
 
 @app.route('/config')
 def get_config():
-    check_create_config_file()
-    
     try:
-        with open(CONFIG_FILE_PATH, 'r') as file:
-            content = file.read()
-            venda_value = 0
-            compra_value = 0
-            for line in content.splitlines():
-                if 'Venda =' in line:
-                    venda_value = float(line.split('=')[1].strip().replace('"', '') or 0)
-                if 'Compra =' in line:
-                    compra_value = float(line.split('=')[1].strip().replace('"', '') or 0)
-        return jsonify({"venda": venda_value, "compra": compra_value})
+        # Retorna os valores de "Venda" e "Compra" como JSON
+        return jsonify({"venda": VENDA_VALUE, "compra": COMPRA_VALUE})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route('/check_code', methods=['POST'])
+def check_code():
+    data = request.get_json()
+    code = data.get('code')
+
+    if code == ADMIN_CODE:
+        return jsonify({"status": "success", "message": "Código correto"})
+    else:
+        return jsonify({"status": "failure", "message": "Código incorreto"})
+
 @app.route('/')
 def serve_index():
-    return send_from_directory(BASE_DIR, 'index.html')
+    return send_from_directory(os.getcwd(), 'index.html')
 
 if __name__ == '__main__':
-    check_create_config_file()  # Verifica e cria o arquivo config.txt se necessário
     app.run(debug=True)
